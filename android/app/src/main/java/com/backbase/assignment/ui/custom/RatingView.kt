f21a1b270985fa72ad4d44a5f094b55b183e9f01
oblessing <oniyideinc@gmail.com>
2021-04-08 17:16:12 +0300

    Add RatingView

--- android/app/src/main/java/com/backbase/assignment/ui/custom/RatingView.kt
+++ android/app/src/main/java/com/backbase/assignment/ui/custom/RatingView.kt
@@ -2,8 +2,32 @@ package com.backbase.assignment.ui.custom
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.View
+import android.widget.FrameLayout
+import androidx.core.content.ContextCompat
+import com.backbase.assignment.R
+import com.backbase.assignment.core.mavericks.viewBinding
+import com.backbase.assignment.databinding.ViewRatingBinding
 
 class RatingView @JvmOverloads constructor(
-    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
-) : View(context, attrs, defStyleAttr)
+        context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+    private val binding: ViewRatingBinding by viewBinding()
+
+    fun setRating(value: Double) {
+        val percentage = (value * 10).toInt()
+        binding.indicatorView.max = 100
+        binding.indicatorView.progress = percentage
+        binding.indicatorView.isIndeterminate = false
+        binding.valueView.text = "$percentage"
+
+        if (percentage >= 50) {
+            // paint green
+            binding.indicatorView.background.setTint(ContextCompat.getColor(context, R.color.colorDarkGreen))
+            binding.indicatorView.progressDrawable.setTint(ContextCompat.getColor(context, R.color.colorLightGreen))
+        } else {
+            // paint yellow
+            binding.indicatorView.background.setTint(ContextCompat.getColor(context, R.color.colorDarkYellow))
+            binding.indicatorView.progressDrawable.setTint(ContextCompat.getColor(context, R.color.colorLightYellow))
+        }
+    }
+}
